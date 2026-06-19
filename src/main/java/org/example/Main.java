291654662791aec3a5f846b61634abeef01aefb5
Rajan-91480 <rajan91480@gmail.com>
2026-06-19 21:08:22 +0530

    1st commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hellow World!");
+        System.out.println("devops program 3");
+    }
+}
